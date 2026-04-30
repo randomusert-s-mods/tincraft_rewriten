bd91e814f348685721c8fac85a6d512d9ebc45fb
randomusert <eetupajamaki@gmail.com>
2026-04-30 19:14:34 +0300

    work on the tin generator block

--- common/src/main/java/io/github/randomusert/mods/tincraft/block/TinGeneratorBlock.java
+++ common/src/main/java/io/github/randomusert/mods/tincraft/block/TinGeneratorBlock.java
@@ -1,10 +1,66 @@
 package io.github.randomusert.mods.tincraft.block;
 
-import net.minecraft.world.level.block.Block;
+import com.mojang.serialization.MapCodec;
+import dev.architectury.event.EventResult;
+import dev.architectury.event.events.common.InteractionEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+import org.jetbrains.annotations.Nullable;
 
-public class TinGeneratorBlock extends Block {
+public class TinGeneratorBlock extends BaseEntityBlock implements InteractionEvent.RightClickBlock {
+    public static final DirectionProperty FACING;
+
+    public static final MapCodec<TinGeneratorBlock> CODEC = simpleCodec(TinGeneratorBlock::new);
 
     public TinGeneratorBlock(Properties props) {
         super(props);
+
+        this.registerDefaultState((BlockState)((BlockState)((BlockState)this.stateDefinition.any()).setValue(FACING, Direction.NORTH)));
+        InteractionEvent.RIGHT_CLICK_BLOCK.register(this);
+    }
+
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return null;
+    }
+
+    @Override
+    public EventResult click(Player player, InteractionHand hand, BlockPos pos, Direction face) {
+        return null;
+    }
+
+    protected RenderShape getRenderShape(BlockState blockState) {
+        return RenderShape.MODEL;
+    }
+
+    protected BlockState rotate(BlockState blockState, Rotation rotation) {
+        return (BlockState)blockState.setValue(FACING, rotation.rotate((Direction)blockState.getValue(FACING)));
+    }
+
+    protected BlockState mirror(BlockState blockState, Mirror mirror) {
+        return blockState.rotate(mirror.getRotation((Direction)blockState.getValue(FACING)));
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(new Property[]{FACING});
+    }
+
+    static {
+        FACING = HorizontalDirectionalBlock.FACING;
+
+    }
+
+    @Override
+    public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return null;
     }
 }
