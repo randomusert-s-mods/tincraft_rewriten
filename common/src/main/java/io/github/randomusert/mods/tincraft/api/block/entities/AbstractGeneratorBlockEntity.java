cd162b0436df1c777b637ef6e93923861eea1c12
randomusert <eetupajamaki@gmail.com>
2026-05-02 12:24:54 +0300

    basic half not implemented AbstractGeneratorBlockEntity class

--- common/src/main/java/io/github/randomusert/mods/tincraft/api/block/entities/AbstractGeneratorBlockEntity.java
+++ common/src/main/java/io/github/randomusert/mods/tincraft/api/block/entities/AbstractGeneratorBlockEntity.java
@@ -1,4 +1,112 @@
 package io.github.randomusert.mods.tincraft.api.block.entities;
 
-public class AbstractGeneratorBlockEntity {
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.WorldlyContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.RecipeCraftingHolder;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+public class AbstractGeneratorBlockEntity extends BaseContainerBlockEntity implements WorldlyContainer, RecipeCraftingHolder {
+
+
+    /// -----VARIABLES-----
+    ///
+    /// These variables are used to define internal factors to the block
+    ///
+    /// These don't need to be over writen as these values are base values
+    protected static final int SLOT_INPUT = 0;
+    protected NonNullList<ItemStack> items;
+    int capacity;
+    int maxOut;
+    int currentEnergy;
+    int maxBurnLength;
+
+
+    //-----CONSTRUCTOR-----
+    protected AbstractGeneratorBlockEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
+        super(blockEntityType, blockPos, blockState);
+    }
+
+   /// A consumer must @override this method
+   ///
+   /// Gets the default name as the name suggests
+    @Override
+    protected Component getDefaultName() {
+        return null;
+    }
+
+
+    @Override
+    protected NonNullList<ItemStack> getItems() {
+        return this.items;
+    }
+
+    @Override
+    protected void setItems(NonNullList<ItemStack> nonNullList) {
+        this.items = nonNullList;
+    }
+
+    /// Creates a menu
+    ///
+    /// Must be annotated with @override
+    @Override
+    protected AbstractContainerMenu createMenu(int i, Inventory inventory) {
+        return null;
+    }
+
+    @Override
+    public int getContainerSize() {
+        return 0;
+    }
+
+    @Override
+    public int getMaxStackSize() {
+        return super.getMaxStackSize();
+    }
+
+    @Override
+    public int[] getSlotsForFace(Direction direction) {
+        return new int[0];
+    }
+
+    @Override
+    public boolean canPlaceItemThroughFace(int i, ItemStack itemStack, @Nullable Direction direction) {
+        return false;
+    }
+
+    @Override
+    public boolean canTakeItemThroughFace(int i, ItemStack itemStack, Direction direction) {
+        return false;
+    }
+
+    @Override
+    public void setRecipeUsed(@Nullable RecipeHolder<?> recipeHolder) {
+
+    }
+
+    @Override
+    public @Nullable RecipeHolder<?> getRecipeUsed() {
+        return null;
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag compoundTag, HolderLookup.Provider provider) {
+        super.loadAdditional(compoundTag, provider);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag compoundTag, HolderLookup.Provider provider) {
+        super.saveAdditional(compoundTag, provider);
+    }
 }
